34158ac549ee3c54955f603665f3a7b7a16deb85
mbasa <mario.basa@gmail.com>
2021-12-23 11:38:13 +0900

    fixed overflow problem when keyboard appears.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -31,149 +31,148 @@ class LoginPageState extends State<LoginPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "Login",
-                  style: TextStyle(fontSize: 18.0),
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(200, 200, 201, 1.0),
-                    borderRadius: BorderRadius.circular(16.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 22,
                   ),
-                  child: TextFormField(
-                    obscureText: false,
-                    validator: (v) {
-                      if (v == null || v.isEmpty) {
-                        return "Field can not be empty";
-                      }
-                      _user = v;
-                      return null;
-                    },
-                    //style: TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      hintText: 'Username',
-                      hintStyle: TextStyle(color: Colors.white),
-                      labelStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(200, 200, 201, 1.0),
+                      borderRadius: BorderRadius.circular(16.0),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(200, 200, 201, 1.0),
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  child: TextFormField(
-                    obscureText: true,
-                    validator: (v) {
-                      if (v == null || v.isEmpty) {
-                        return "Field can not be empty";
-                      }
-                      _password = v;
-                      return null;
-                    },
-                    //style: TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Colors.white),
-                      labelStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(200, 200, 201, 1.0),
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  child: TextFormField(
-                    keyboardType: TextInputType.url,
-                    obscureText: false,
-                    validator: (v) {
-                      if (v == null || v.isEmpty) {
-                        return "Field can not be empty";
-                      } else if (!Uri.parse(v).isAbsolute) {
-                        return "Not a valid URL Address";
-                      }
-                      _url = v;
-                      return null;
-                    },
-                    //style: TextStyle(color: Colors.white),
-                    decoration: const InputDecoration(
-                      hintText: 'Redmine Server URL',
-                      hintStyle: TextStyle(color: Colors.white),
-                      labelStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 35,
-                ),
-                SizedBox(
-                  width: 160,
-                  height: 40,
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                          shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16.0),
-                                  side: const BorderSide(color: Colors.red)))),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          DialogUtil.showOnSendDialog(
-                              context, "Sending Login Information");
-
-                          String apiToken = await NetworkHelper.getApiKey(
-                              _user, _password, _url);
-
-                          Navigator.pop(context);
-
-                          if (apiToken.isNotEmpty) {
-                            debugPrint("Registered: $apiToken");
-
-                            _dbh.insertUserCredential(
-                                _user, _password, _url, apiToken);
-
-                            Navigator.pushAndRemoveUntil(context,
-                                MaterialPageRoute(
-                              builder: (context) {
-                                return const MainPage();
-                              },
-                            ), (route) => false);
-                          } else {
-                            DialogUtil.showCustomDialog(
-                                context,
-                                "Error",
-                                "An Error has Occurred. \nCheck entered parameters.",
-                                "Close");
-                          }
+                    child: TextFormField(
+                      obscureText: false,
+                      validator: (v) {
+                        if (v == null || v.isEmpty) {
+                          return "Field can not be empty";
                         }
+                        _user = v;
+                        return null;
                       },
-                      child: const Text("Send")),
-                ),
-              ],
+                      //style: TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: TextStyle(color: Colors.white),
+                        labelStyle: TextStyle(color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(200, 200, 201, 1.0),
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: TextFormField(
+                      obscureText: true,
+                      validator: (v) {
+                        if (v == null || v.isEmpty) {
+                          return "Field can not be empty";
+                        }
+                        _password = v;
+                        return null;
+                      },
+                      //style: TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.white),
+                        labelStyle: TextStyle(color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(200, 200, 201, 1.0),
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: TextFormField(
+                      keyboardType: TextInputType.url,
+                      obscureText: false,
+                      validator: (v) {
+                        if (v == null || v.isEmpty) {
+                          return "Field can not be empty";
+                        } else if (!Uri.parse(v).isAbsolute) {
+                          return "Not a valid URL Address";
+                        }
+                        _url = v;
+                        return null;
+                      },
+                      //style: TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        hintText: 'Redmine Server URL',
+                        hintStyle: TextStyle(color: Colors.white),
+                        labelStyle: TextStyle(color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  SizedBox(
+                    width: 160,
+                    height: 40,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    side:
+                                        const BorderSide(color: Colors.red)))),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            DialogUtil.showOnSendDialog(
+                                context, "Sending Login Information");
+
+                            String apiToken = await NetworkHelper.getApiKey(
+                                _user, _password, _url);
+
+                            Navigator.pop(context);
+
+                            if (apiToken.isNotEmpty) {
+                              debugPrint("Registered: $apiToken");
+
+                              _dbh.insertUserCredential(
+                                  _user, _password, _url, apiToken);
+
+                              Navigator.pushAndRemoveUntil(context,
+                                  MaterialPageRoute(
+                                builder: (context) {
+                                  return const MainPage();
+                                },
+                              ), (route) => false);
+                            } else {
+                              DialogUtil.showCustomDialog(
+                                  context,
+                                  "Error",
+                                  "An Error has Occurred. \nCheck entered parameters.",
+                                  "Close");
+                            }
+                          }
+                        },
+                        child: const Text("Login")),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
